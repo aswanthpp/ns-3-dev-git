8607e0247b697624456f7bc0b44c698acba3d61d
arvind97 <arvind0705.ar@gmail.com>
2017-12-01 16:49:02 +0530

    Code indentation dhcp-example-relay.cc

--- src/internet-apps/examples/dhcp-example-relay.cc
+++ src/internet-apps/examples/dhcp-example-relay.cc
@@ -33,145 +33,107 @@
 
 using namespace ns3;
 
-/*Define a Log component with the name DhcpExample*/
 NS_LOG_COMPONENT_DEFINE ("DhcpExampleRelay");
 
-int main (int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
-  bool verbose = false;
-  bool tracing = false;
-  Time stopTime = Seconds (20);
+	bool verbose = false;
+	bool tracing = false;
+	Time stopTime = Seconds (20);
 
-  CommandLine cmd;
-  cmd.AddValue ("verbose", "turn on the logs", verbose);
-  cmd.AddValue ("tracing", "turn on the tracing", tracing);
-  /*Parse the program arguments*/
-  cmd.Parse (argc, argv);
+	CommandLine cmd;
+	cmd.AddValue ("verbose", "turn on the logs", verbose);
+	cmd.AddValue ("tracing", "turn on the tracing", tracing);
 
-  // GlobalValue::Bind ("ChecksumEnabled", BooleanValue (true));
+	cmd.Parse (argc, argv);
 
-  if (verbose)
-    {
-      LogComponentEnable ("DhcpServer", LOG_LEVEL_ALL);
-      LogComponentEnable ("DhcpClient", LOG_LEVEL_ALL);
-     //LogComponentEnable ("DhcpRelay", LOG_LEVEL_ALL);
-      //LogComponentEnable ("DhcpExampleRelay", LOG_LEVEL_ALL);
-      //LogComponentEnable ("DhcpHelper", LOG_LEVEL_ALL);
+	// GlobalValue::Bind ("ChecksumEnabled", BooleanValue (true));
 
-    }
+	if (verbose)
+		{
+			LogComponentEnable ("DhcpServer", LOG_LEVEL_ALL);
+			LogComponentEnable ("DhcpClient", LOG_LEVEL_ALL);
+		  //LogComponentEnable ("DhcpRelay", LOG_LEVEL_ALL);
+		}
 
-  NS_LOG_INFO ("Create nodes.");
-  /*NodeContainer - Keeps track of a set of node pointers. Typically ns-3 helpers operate on more than one node at 
-  a time. For example, a device helper may want to install devices on a large number of similar nodes. The helper 
-  Install methods usually take a NodeContainer as a parameter. NodeContainers hold the multiple Ptr<Node> which 
-  are used to refer to the nodes*/
-  NodeContainer nodes;
-  NodeContainer relay;
-  
-  /*Create 3 nodes and append pointers to them to the end of this NodeContainer, i.e.,nodes*/
-  nodes.Create (3);
-  relay.Create (1);
+	NS_LOG_INFO ("Create nodes.");
+	NodeContainer nodes;
+	NodeContainer relay;
+	nodes.Create (3);
+	relay.Create (1);
 
-  /*Create a node container which is a concatenation of two input NodeContainers*/
-  NodeContainer net (nodes, relay);
+	NodeContainer net (nodes, relay);
 
-  NS_LOG_INFO ("Create channels.");
+	NS_LOG_INFO ("Create channels.");
+	CsmaHelper csma;
+	csma.SetChannelAttribute ("DataRate", StringValue ("5Mbps"));
+	csma.SetChannelAttribute ("Delay", StringValue ("2ms"));
+	csma.SetDeviceAttribute ("Mtu", UintegerValue (1500));
+	NetDeviceContainer devNet = csma.Install (net);
 
-  /*Build a set of CsmaNetDevice objects*/
-  CsmaHelper csma;
+	NodeContainer p2pNodes;
+	p2pNodes.Add (net.Get (3));
+	p2pNodes.Create (1);
 
-  /*SetChannelAttribute - Set these attributes on each ns3::CsmaChannel created by CsmaHelper::Install*/
-  csma.SetChannelAttribute ("DataRate", StringValue ("5Mbps"));
-  csma.SetChannelAttribute ("Delay", StringValue ("2ms"));
+	PointToPointHelper pointToPoint;
+	pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+	pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
-  /*SetDeviceAttribute - Set these attributes on each ns3::CsmaNetDevice created by CsmaHelper::Install*/
-  csma.SetDeviceAttribute ("Mtu", UintegerValue (1500));
+	NetDeviceContainer p2pDevices;
+	p2pDevices = pointToPoint.Install (p2pNodes);
 
-  /*Install - This method creates an ns3::CsmaChannel with the attributes configured by 
-  CsmaHelper::SetChannelAttribute, an ns3::CsmaNetDevice with the attributes configured by 
-  CsmaHelper::SetDeviceAttribute and then adds the device to the node, i.e., net and attaches the 
-  channel to the device*/
-  NetDeviceContainer devNet = csma.Install (net);
+	InternetStackHelper tcpip;
+	tcpip.Install (nodes);
+	tcpip.Install (relay);
+	tcpip.Install (p2pNodes.Get (1));
 
-  NodeContainer p2pNodes;
+	NS_LOG_INFO ("Setup the IP addresses and create DHCP applications.");
+	DhcpHelper dhcpHelper;
 
-  /*Add - Append the contents of another NodeContainer to the end of this container*/
-  p2pNodes.Add (net.Get (3));
-  p2pNodes.Create (1);
+	Ipv4InterfaceContainer relayClient = dhcpHelper.InstallFixedAddress (devNet.Get (3), 
+																																			Ipv4Address ("172.30.0.17"), 
+																																			Ipv4Mask ("/24"));   
 
-  /*PointToPointHelper - Build a set of PointToPointNetDevice objects*/
-  PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+	// DHCP server
+	ApplicationContainer dhcpServerApp = dhcpHelper.InstallDhcpServer (p2pDevices.Get (1), Ipv4Address ("172.30.1.12"),
+																																		 Ipv4Address ("172.30.1.0"), Ipv4Mask ("/24"),
+																																		 Ipv4Address ("172.30.1.10"), 
+																																		 Ipv4Address ("172.30.1.15"));
 
-  NetDeviceContainer p2pDevices;
-  p2pDevices = pointToPoint.Install (p2pNodes);
+	//DHCP Relay Agent
+	ApplicationContainer dhcpRelayApp = dhcpHelper.InstallDhcpRelay (p2pDevices.Get (0), Ipv4Address ("172.30.1.16"),
+																																	Ipv4Mask ("/24"), Ipv4Address ("172.30.1.12"));
 
-  /*InternetStackHelper - Aggregate IP/TCP/UDP functionality to existing Nodes*/
-  InternetStackHelper tcpip;
+	// This is just to show how it can be done.
+	DynamicCast<DhcpServer> (dhcpServerApp.Get (0))->AddStaticDhcpEntry (devNet.Get (2)->GetAddress (), Ipv4Address ("172.30.1.14"));
 
-  /*Install - Aggregate implementations of the ns3::Ipv4, ns3::Ipv6, ns3::Udp, and ns3::Tcp classes onto the 
-  provided node*/
-  tcpip.Install (nodes);
-  tcpip.Install (relay);
-  tcpip.Install (p2pNodes.Get (1));
+	dhcpServerApp.Start (Seconds (0.0));
+	dhcpServerApp.Stop (stopTime);
 
-  NS_LOG_INFO ("Setup the IP addresses and create DHCP applications.");
+	dhcpRelayApp.Start (Seconds (0.0));
+	dhcpRelayApp.Stop (stopTime);
 
-  /*DhcpHelper - The helper class used to configure and install DHCP applications on nodes*/
-  DhcpHelper dhcpHelper;
+	// DHCP clients
+	NetDeviceContainer dhcpClientNetDevs;
+	dhcpClientNetDevs.Add (devNet.Get (0));
+	dhcpClientNetDevs.Add (devNet.Get (1));
+	dhcpClientNetDevs.Add (devNet.Get (2));
 
-  /*Ipv4InterfaceContainer - holds a vector of std::pair of Ptr<Ipv4> and interface index*/
-  Ipv4InterfaceContainer relayClient = dhcpHelper.InstallFixedAddress (devNet.Get (3), 
-                                                                      Ipv4Address ("172.30.0.17"), 
-                                                                      Ipv4Mask ("/24"));   
+	ApplicationContainer dhcpClients = dhcpHelper.InstallDhcpClient (dhcpClientNetDevs);
+	dhcpClients.Start (Seconds (1.0));
+	dhcpClients.Stop (stopTime);
 
-  //NS_LOG_INFO("-----relayClient : "<<relayClient.Get (0).first<<"----------");
-  
-  // DHCP server
-  /*ApplicationContainer - holds a vector of ns3::Application pointers
-  InstallDhcpServer(netDevice, serverAddr, poolAddr, poolMask, minAddr, maxAddr, gateway)*/
-  ApplicationContainer dhcpServerApp = dhcpHelper.InstallDhcpServer (p2pDevices.Get (1), Ipv4Address ("172.30.1.12"),
-                                                                     Ipv4Address ("172.30.1.0"), Ipv4Mask ("/24"),
-                                                                     Ipv4Address ("172.30.1.10"), 
-                                                                     Ipv4Address ("172.30.1.15"));
+	Simulator::Stop (stopTime + Seconds (10.0));
 
-  /*InstallDhcpRelay (netDevice, relayAddr,subMask, dhcps)*/
-  ApplicationContainer dhcpRelayApp = dhcpHelper.InstallDhcpRelay (p2pDevices.Get (0), Ipv4Address ("172.30.1.16"),
-                                                                  Ipv4Mask ("/24"), Ipv4Address ("172.30.1.12"));
+	if (tracing)
+		{
+			csma.EnablePcapAll ("dhcp-csma");
+			pointToPoint.EnablePcapAll ("dhcp-p2p");
+		}
 
-  // This is just to show how it can be done.
-  DynamicCast<DhcpServer> (dhcpServerApp.Get (0))->AddStaticDhcpEntry (devNet.Get (2)->GetAddress (), 
-                           Ipv4Address ("172.30.1.14"));
-
-  /*Start - This method simply iterates through the contained Applications and calls their Start() 
-  methods with the provided Time*/
-  dhcpServerApp.Start (Seconds (0.0));
-  dhcpServerApp.Stop (stopTime);
-
-  dhcpRelayApp.Start (Seconds (0.0));
-  dhcpRelayApp.Stop (stopTime);
-
-  // DHCP clients
-  NetDeviceContainer dhcpClientNetDevs;
-  dhcpClientNetDevs.Add (devNet.Get (0));
-  dhcpClientNetDevs.Add (devNet.Get (1));
-  dhcpClientNetDevs.Add (devNet.Get (2));
-
-  ApplicationContainer dhcpClients = dhcpHelper.InstallDhcpClient (dhcpClientNetDevs);
-  dhcpClients.Start (Seconds (1.0));
-  dhcpClients.Stop (stopTime);
-
-  Simulator::Stop (stopTime + Seconds (10.0));
-
-  if (tracing)
-    {
-      csma.EnablePcapAll ("dhcp-csma");
-      pointToPoint.EnablePcapAll ("dhcp-p2p");
-    }
-
-  NS_LOG_INFO ("Run Simulation.");
-  Simulator::Run ();
-  Simulator::Destroy ();
-  NS_LOG_INFO ("Done.");
+	NS_LOG_INFO ("Run Simulation.");
+	Simulator::Run ();
+	Simulator::Destroy ();
+	NS_LOG_INFO ("Done.");
 }
