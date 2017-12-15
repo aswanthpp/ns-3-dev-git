f904c9b89317534d006ff74653108558b0fd09e1
ASWANTH <aswanth@hp-notebook.nitk.ac.in>
2017-12-15 23:33:08 +0530

    echo server added

--- src/internet-apps/examples/dhcp-example-relay.cc
+++ src/internet-apps/examples/dhcp-example-relay.cc
@@ -14,14 +14,13 @@ NS_LOG_COMPONENT_DEFINE ("DhcpExampleRelay");
 int
 main (int argc, char *argv[])
 {
+	CommandLine cmd;
+
 	bool verbose = false;
 	bool tracing = false;
-	Time stopTime = Seconds (20);
-
-	CommandLine cmd;
 	cmd.AddValue ("verbose", "turn on the logs", verbose);
 	cmd.AddValue ("tracing", "turn on the tracing", tracing);
-
+	
 	cmd.Parse (argc, argv);
 
 	// GlobalValue::Bind ("ChecksumEnabled", BooleanValue (true));
@@ -30,8 +29,12 @@ main (int argc, char *argv[])
 		{
 			LogComponentEnable ("DhcpServer", LOG_LEVEL_ALL);
 			LogComponentEnable ("DhcpClient", LOG_LEVEL_ALL);
-		    LogComponentEnable ("DhcpRelay", LOG_LEVEL_ALL);
+		   // LogComponentEnable ("DhcpRelay", LOG_LEVEL_ALL);
+		    LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+      		LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
 		}
+
+	Time stopTime = Seconds (20);
 
 	NS_LOG_INFO ("Create nodes.");
 	NodeContainer nodes;
@@ -59,10 +62,18 @@ main (int argc, char *argv[])
 	NetDeviceContainer p2pDevices;
 	p2pDevices = pointToPoint.Install (p2pNodes);
 
+
+
 	InternetStackHelper tcpip;
 	tcpip.Install (nodes);
 	tcpip.Install (relay);
 	tcpip.Install (p2pNodes.Get (1));
+
+	 Ipv4AddressHelper address;
+  address.SetBase ("172.30.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer p2pInterfaces;
+  p2pInterfaces = address.Assign (p2pDevices);
+
 
 	NS_LOG_INFO ("Setup the IP addresses and create DHCP applications.");
 	DhcpHelper dhcpHelper;
@@ -102,6 +113,22 @@ main (int argc, char *argv[])
 	ApplicationContainer dhcpClients = dhcpHelper.InstallDhcpClient (dhcpClientNetDevs);
 	dhcpClients.Start (Seconds (1.0));
 	dhcpClients.Stop (stopTime);
+
+
+	UdpEchoServerHelper echoServer (9);
+
+  ApplicationContainer serverApps = echoServer.Install (p2pNodes.Get (1));
+  serverApps.Start (Seconds (0.0));
+  serverApps.Stop (stopTime);
+
+  UdpEchoClientHelper echoClient (p2pInterfaces.GetAddress (1), 9);
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (100));
+  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+
+  ApplicationContainer clientApps = echoClient.Install (nodes.Get (1));
+  clientApps.Start (Seconds (10.0));
+  clientApps.Stop (stopTime);
 
 	Simulator::Stop (stopTime + Seconds (10.0));
 
